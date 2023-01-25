bfc442e30101ec3e0d92d150d42205916f99709a
Abm32 <110992125+Abm32@users.noreply.github.com>
2023-01-25 21:24:39 +0530

    On development

---
+++ blog-site/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './App.css';
+ 
+import {
+ createBrowserRouter,
+ RouterProvider,
+} from "react-router-dom";
+import BlogsListPage from './pages/BlogsListPage';
+import BlogPage from './pages/BlogPage';
+ 
+const router = createBrowserRouter([
+  {
+   path: "/",
+   element: <BlogsListPage />,
+  },
+  {
+    path: "/blog",
+    element: <BlogPage />,
+  }
+  
+]);
+ 
+ReactDOM.createRoot(document.getElementById("root")).render(
+ <RouterProvider router={router} />
+);

8ef1bc74eddf85694a7573f6344137a4c4743f90
Abm32 <110992125+Abm32@users.noreply.github.com>
2023-01-26 17:55:25 +0530

    updated

--- blog-site/src/blogs.js
+++ blog-site/src/blogs.js
@@ -1,8 +1,8 @@
 export const my_blogs = {
     "blog_1":{
-      "title":"My First Computer Program",
-      "author":"le stackie",
+      "title":"First Images from the James Webb Space Telescope",
+      "author":"Credits: NASA, ESA, CSA, and STScI",
       "date":"2021-03-01",
-      "content":"I started my programming journey when I was in secondary school. Back then, I joined an Infocomm club and I started to learn Scratch, a versatile drag-and-drop tool that teaches me simple logic flow, data conditioning and simple iteration loops. It got me so interested in programming that I spent hours making a flight simulator on Scratch.\n\nI was so into programming that I took the computing class for my GCE O Levels. There, I started learning my first real programming language and that was Python. I was learning data structuring, the basics of networking, functions and classes and much more. My computing teacher gave my classes a series of programming questions in a gamified manner. Each time we got an algorithm correct, we were awarded points. My friends and I will spend time after school trying to outdo each other.\n\nWith the experience I had, and the opportunities I was given, I decided to pursue a career in programming. I am currently studying Computer Science at the National University of Singapore. I am currently in my 3rd year and I am looking forward to my final year project and internship opportunities.",
+      "content":"This landscape of “mountains” and “valleys” speckled with glittering stars is actually the edge of a nearby, young, star-forming region called NGC 3324 in the Carina Nebula. Captured in infrared light by NASA’s new James Webb Space Telescope, this image reveals for the first time previously invisible areas of star birth. Called the Cosmic Cliffs, Webb’s seemingly three-dimensional picture looks like craggy mountains on a moonlit evening. In reality, it is the edge of the giant, gaseous cavity within NGC 3324, and the tallest “peaks” in this image are about 7 light-years high. The cavernous area has been carved from the nebula by the intense ultraviolet radiation and stellar winds from extremely massive, hot, young stars located in the center of the bubble, above the area shown in this image.",
     },
   }
--- blog-site/src/pages/BlogsListPage.js
+++ blog-site/src/pages/BlogsListPage.js
@@ -1,24 +1,27 @@
 import Header from "../components/Header";
 import { my_blogs } from "../blogs";
-import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const BlogPage = () => {
-  let {state} = useLocation()
-  let blog_id = state.blog_id;
-
+const BlogsListPage = () => {
+  let navigate = useNavigate()
   return (
     <div>
       <Header />
       <div className="container">
         <div className="inner-container">
-          <h1 className="page-title">{my_blogs[blog_id].title}</h1>
-          <div className="blog-subcontents">
-            <p className="blog-author">{my_blogs[blog_id].author}</p>
-            <p className="blog-date">{my_blogs[blog_id].date}</p>
-          </div>
-          <img src="./img/header.jpg" className="hero-image" alt="" />
-          <div className="blog-content">
-            <p className="blog-text">{my_blogs[blog_id].content}</p>
+          <h1 className="page-title">My Blogs</h1>
+          <div className="blog-list">
+            {Object.keys(my_blogs).map((blog, index) => (
+              <div className="blog-preview" key={index} onClick={()=>{
+                navigate(`/blog`, {state: {blog_id: blog}})
+              }} >
+                <div className="blog-preview-left">
+                  <h2 className="blog-title">{my_blogs[blog].title}</h2>
+                  <p className="blog-author">{my_blogs[blog].author}</p>
+                </div>
+                <p className="blog-date">{my_blogs[blog].date}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
@@ -26,4 +29,4 @@ const BlogPage = () => {
   );
 }
 
-export default BlogPage;
+export default BlogsListPage;
